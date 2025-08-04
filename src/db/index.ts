859b3c2c043471d913b7336caa44e813293cc3e2
nopaldonh <nopaldonh21@gmail.com>
2025-08-05 01:31:16 +0700

    4.3 Better Auth DB tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,11 +4,12 @@ import mysql from 'mysql2/promise'
 import * as schema from '@/db/schema'
 
 const connection = await mysql.createConnection({
-  uri: process.env.DATABASE_URL as string,
+  uri: process.env.DATABASE_URL,
 })
 
 export const db = drizzle({
   client: connection,
   schema,
+  mode: 'default',
   casing: 'snake_case',
 })
