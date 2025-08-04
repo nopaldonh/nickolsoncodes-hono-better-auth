44c998c8b1b5a06811c9ae2588a74ad6b5eff9d4
nopaldonh <nopaldonh21@gmail.com>
2025-08-05 01:45:35 +0700

    4.4 Better Auth routes & open API plugin

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { auth } from './lib/auth'
 
 const app = new Hono()
+
+app.on(['POST', 'GET'], '/api/auth/**', (c) => auth.handler(c.req.raw))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  }
+)
