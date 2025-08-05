dc47a345d2cf1aff4f06262b995942244824aa86
nopaldonh <nopaldonh21@gmail.com>
2025-08-05 14:28:44 +0700

    6.1 Better Auth email config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { db } from '@/db'
 import { openAPI } from 'better-auth/plugins'
+import { sendEmail } from './email'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -9,6 +10,15 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      await sendEmail({
+        to: user.email,
+        subject: 'Verify your email address',
+        text: `Click the link to verify your email: ${url}`,
+      })
+    },
   },
   plugins: [openAPI()],
 })
