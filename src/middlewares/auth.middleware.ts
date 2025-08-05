5d85eaa96d615365d6ffbcacb9e1c9242a7671de
nopaldonh <nopaldonh21@gmail.com>
2025-08-05 22:21:55 +0700

    7.4 Auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,15 @@
+import type { HonoEnv } from '@/types'
+import { createMiddleware } from 'hono/factory'
+import { auth } from '@/lib/auth'
+
+export const authMiddleware = createMiddleware<HonoEnv>(async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers })
+
+  if (!session) {
+    return c.json({ error: 'Unauthorized' }, 401)
+  }
+
+  c.set('user', session.user)
+  c.set('session', session.session)
+  return next()
+})
